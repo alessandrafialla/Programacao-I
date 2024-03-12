6923e625df1c9ac89db84d272b4963968a0dde16
alessandrafialla <alessandrafialla@ufpr.br>
2024-03-12 09:19:49 -0300

    exercicios

---
+++ aq2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int num1, num2; 
+
+    while(1)
+    {
+        scanf("%d", &num1);
+        
+        if(num1 == 0)
+            printf("%d", 0);
+            return 0;
+
+            scanf("%d", &num2);
+            if (num1 * num1 != num2)
+                printf("%d", 0);
+                return 0;
+        
+    }
+
+    
--- aquecimento1.c
+++ aquecimento1.c
@@ -3,12 +3,13 @@
 int main()
 {
 	int n;
-
 	scanf("%d", &n);
 	
-	/*i vai ate n/2 para reduzir a quantidade de comparacoes */
-	for (int i = 0; i < n/2 ; i++){
-		if (i * (i + 1) * (i + 2) == n){
+	/*i vai ate n/3 para reduzir a quantidade de comparacoes */
+	for (int i = 0; i < n/3 ; i++)
+	{
+		if (i * (i + 1) * (i + 2) == n) /* maior valor possivel de i eh n/3*/
+		{
 			printf ("%d\n" , 1);
 			return 0;
 		}
