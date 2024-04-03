a1f97d4e56d393beec8b19283f7c843186086673
alessandrafialla <alessandrafialla@ufpr.br>
2024-04-02 22:55:29 -0300

    Trabalho extra concluido, falta testar com mais valores

--- trabalho_extra/main.c
+++ trabalho_extra/main.c
@@ -4,29 +4,34 @@
 
 #define MAX 100
 
-void elimina_invalidos (v, n){
+/* Elimina valores invalidos substituindo pelo ultimo valor do vetor */
+/* Modifica o tamanho de n                                           */
+void elimina_invalidos (struct racional *v, int *n){
     int i;
-
-    for (i = 0; i < n; i++){
-        if ( !valido_r (v[i]){
-                v[i] = v[n-1];
-                n--; //decrementa tamanho do vetor
+    
+    for (i = 0; i < (*n); i++){
+        if ( !valido_r (v[i]) ){
+                v[i] = v[(*n)-1]; //troca com o ultimo valor
+                (*n)--; 
                 i--; // decrementa i para testar o substituto
+        }
     }
 }
 
-void imprime_racionais (v, n){
+/* Imprime todos os racionais do vetor */
+void imprime_vetor_racionais (struct racional *v, int n){
     int i;
 
     /* Imprime os racionais contidos no vetor*/
     for (i = 0; i < n; i++){
         imprime_r(v[i]);
-        if (i < n-1)
+        if (i < n-1) // nao imprime o espaco no ultimo elemento
             printf (" ");
     }
     printf ("\n");
 }
 
+/*Funcao principal*/
 int main(){
     int n, i;
     int num, den;
@@ -41,10 +46,11 @@ int main(){
         v[i] = cria_r (num, den);
     }
     
-    /*Imprime racionais contidos no vetor*/
-    imprime_racionais (v, n);
-    elimina_invalidos (v, n);
-    imprime_racionais (v, n);
+    /*Imprime racionais contidos no vetor, elimina invalidos e imprime novamente*/
+    imprime_vetor_racionais (v, n);
+    elimina_invalidos (v, &n);
+    imprime_vetor_racionais (v, n);
+
     return 0;
 }
 
