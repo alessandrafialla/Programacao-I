38adcac88ded1d8febb0c2e123c4c309dc20b565
alessandrafialla <alessandrafialla@ufpr.br>
2024-04-09 12:00:13 -0300

    começo

---
+++ tp3/tp3.c
@@ -0,0 +1,17 @@
+/* Arquivo MAIN que usa o TAD racionais */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "racionais.h"
+
+/* coloque funcoes desejadas aqui. 
+ * Exemplo: ordenar, eliminar invalidos... */
+
+int main (){
+    /* vetor de ponteiros para racionais */
+    struct racional **v;  /* equivalente a struct racional *v[] */
+
+    /* coloque seu codigo aqui */
+
+    return 0;
+}
