d22f76b364b35c879aa85f4cba03bf6508b991bd
alessandrafialla <alessandrafialla@ufpr.br>
2024-04-17 01:35:48 -0300

    Arrumar soma dos elementos do vetor

--- tp3/racionais.c
+++ tp3/racionais.c
@@ -15,7 +15,7 @@ int aleat (long int min, long int max){
 /* Calcula o mdc pelo metodo de Euclides.    */
 /* Dica: a versao recursiva eh bem elegante! */
 long int mdc (long int a, long int b){
-    if ((a % b) == 0)
+    if (a % b == 0)
         return b;
     return mdc (b, a % b);
 }
@@ -30,37 +30,136 @@ long int mmc (long int a, long int b){
    Quem chama esta funcao deve garantir que *r seja valido e nao NULL. */
 void simplifica_r (struct racional *r){
     int divisor;
-
+    
+    /* Divide o numerador e o denominador pelo mdc entre eles */
     divisor = mdc (r->num, r->den);
     r->num = r->num / divisor;
     r->den = r->den / divisor;
 
+    /* Se o denominador for negativo migra sinal para numerador */
+    /* Se numerador e denominador forem negativos, tranforma em positivo */
     if (r->den < 0) {
         r->num = r->num * (-1);
         r->den = r->den * (-1);
     }
 }
 
-/* Testar se aloca da forma certa se devolve o endereco */
-/* Depois de usar testar se eh NULL, prob de alocacao*/
+/* Cria e aloca memoria para um racional, se erro retorna NULL */
+/* Retorna o racional apontado na forma simplificada           */
 struct racional *cria_r (long int numerador, long int denominador){
     struct racional *r;
 
+    /*Aloca memoria e testa, se erro retorna NULL*/
     r = malloc (sizeof (struct racional));
+    if (r == NULL)
+        return NULL; 
+
+    /* Inicializa com valores fornecidos e simplifica se valido*/
     r->num = numerador;
     r->den = denominador;
+    if (valido_r(r))
+        simplifica_r (r); 
 
     return r;
 }
 
-/*Libera e aterra ponteiro*/
+/* Libera memoria e aterra ponteiro */
 void destroi_r (struct racional **r){
     free (*r);
     *r = NULL;
 }
 
+/* Retorna 1 se valido e 0 se invalido */
 int valido_r (struct racional *r){
     return r->den != 0;  
+}
+
+/* Retorna o denominador de um racional */
+long int denominador_r (struct racional *r){
+    return r->den;
+}
+
+/* Retorna o numerador de um racional */
+long int numerador_r (struct racional *r){
+    return r->num;
+}
+
+/* Retorna a soma simplificada de *r1 e *r2                      */
+/* Retirna NULL se alocacao deu errado                           */
+struct racional *soma_r (struct racional *r1, struct racional *r2){
+    long int num, den;
+    struct racional *r;
+
+    den = mmc (r1->den, r2->den);
+    num = ((den / r1->den) * r1->num) + ((den / r2->den) * r2->num);
+
+    r = cria_r (num, den); // aloca memoria e retorna simplificado, se possivel
+    if (r == NULL) // cria retorna NULL se tiver erro no malloc
+        return NULL;
+    return r;
+}
+
+/* Retorna a subtracao simplificada dos racionais *r1 e *r2         */
+struct racional *subtrai_r (struct racional *r1, struct racional *r2){
+    long int num, den;
+    struct racional *r;
+
+    den = mmc (r1->den, r2->den);
+    num = ((den / r1->den) * r1->num) - ((den / r2->den) * r2->num);
+
+    r = cria_r (num, den); //aloca memoria e retorna simplificado, se possivel
+    if (r == NULL) //se erro no malloc
+        return NULL;
+    return r;
+}
+
+/* Retorna a multiplicacao simplificada dos racionais *r1 e *r2        */
+struct racional *multiplica_r (struct racional *r1, struct racional *r2){
+    long int num, den;
+    struct racional *r;
+
+    num = (r1->num * r2->num);
+    den = (r2->den * r2->den);
+
+    r = cria_r (num, den); //aloca e retorna simplificado
+    if (r == NULL)
+        return NULL;
+    return r;
+}
+
+/* Retorna a divisao simplificada dps racionais *r1 e *r2   
+ * RetornA NULL se a memoria nao foi alocada corretamente
+ * Retorna NULL se o resultado da divisao for invalido     */
+struct racional *divide_r (struct racional *r1, struct racional *r2){
+    long int num, den;
+    struct racional *r;
+
+    num = (r1->num * r2->den);
+    den = (r1->den * r2->num);
+    r = cria_r (num, den);
+    
+    /* Retorna NULL se ocorreu erro na alocacao ou se resultado eh invalido*/
+    if ((r == NULL) || !valido_r(r))
+        return NULL;
+    return r;
+}
+
+/* Compara dois racionais usando uma fracao equivalente    
+ * Retorna -1 se r1 < r2; 0 se r1 == r2 e 1 se r1 > r2   */
+int compara_r (struct racional *r1, struct racional *r2){
+    long int den, eq1, eq2;
+
+    den = mmc (r1->den, r2->den);// Iguala o denominador dos dois racionais
+    eq1 = ((den / r1->den) * r1->num); // Numerador equivalente de r1
+    eq2 = ((den / r2->den) * r2->num); //Numerador equivalente de r2
+
+    /* Como possuem o mesmo den, compara o numerador*/
+    if (eq1 < eq2)
+        return -1;
+    else if (eq1 == eq2)
+        return 0;
+    else //eq1 > eq2
+        return 1;
 }
 
 /* Imprime um numero racional */
@@ -94,6 +193,7 @@ void imprime_r (struct racional *r) {
     printf ("%ld/%ld" , r->num, r->den);
     return;
 }
+
 /* aqui voce pode definir mais funcoes internas, caso queira ou precise */
 
 /* Implemente aqui as funcoes cujos prototipos estao em racionais.h */
--- tp3/tp3.c
+++ tp3/tp3.c
@@ -4,8 +4,6 @@
 #include <stdlib.h>
 #include "racionais.h"
 
-/* coloque funcoes desejadas aqui. 
- * Exemplo: ordenar, eliminar invalidos... */
 
 /* Elimina valores invalidos substituindo pelo ultimo valor do vetor */
 /* Modifica o valor de n que representa o tamanho do vetor           */
@@ -15,15 +13,15 @@ void elimina_invalidos (struct racional **v, int *n){
     /* Percorre o vetor trocando o valor invalido com valor da ultima posicao*/
     for (i = 0; i < (*n); i++){
         if ( !valido_r (v[i]) ){
-                v[i] = v[(*n)-1];
-                destroi_r (&*v[(*n)-1]); //destroi e aterra a ultima pos
-                (*n)--; 
-                i--; // decrementa i para testar o substituto
+            *v[i] = *v[(*n)-1];
+            destroi_r (&v[(*n)-1]); //destroi e aterra a ultima pos
+            (*n)--; 
+            i--; // decrementa i para testar o substituto
         }
     }
 }
 
-/* Imprime todos os racionais do vetor */
+/* Imprime todos os racionais do vetor                  */
 void imprime_vetor_racionais (struct racional **v, int n){
     int i;
 
@@ -36,33 +34,99 @@ void imprime_vetor_racionais (struct racional **v, int n){
     printf ("\n");
 }
 
+/* Ordena o vetor   */
+void selection_sort (struct racional **v, int n){
+    int menor, i;
+    struct racional *aux;
+
+    /*Considera o primeiro elemento do vetor como menor*/
+    for(i = 0; i < n; i++){
+        menor = i;
+
+        /*Verifica indice do menor valor do vetor*/
+        for (i = 1; i < n; i++){
+            if (compara_r(v[menor], v[i])  == 1){
+                menor = i;
+            }
+            /* Ordena a posicao i do vetor */
+            aux = v[menor];
+            v[menor] = v[i];
+            v[i] = aux;
+        }
+        //free (aux);
+    }
+}
 
 int main (){
 
     /* vetor de ponteiros para racionais */
     struct racional **v;  /* equivalente a struct racional *v[] */
-    int n, i;
+    struct racional *soma;
+    int n, i, j;
     long int num, den;
     
-    srand(0);
+    //srand(0);
 
     /* 0 < n < 100 */
-    scanf( "%d", &n);
+    scanf("%d", &n);
 
-    /*Aloca dinamicamente um vetor[n] de ponteiros para numeros racionais*/
+    /* Aloca dinamicamente um vetor[n] de ponteiros para numeros racionais */
     v = malloc ( n * sizeof(struct racional *)); 
     
     if (v == NULL)
         return 1;
 
-    /*Inicializa vetor com valores do teclado*/
+    /* Inicializa vetor com valores do teclado */
     for (i = 0; i < n; i++){
         scanf("%ld %ld", &num, &den);
         v[i] = cria_r (num, den);
     }
 
-    imprime_vetor_racionais(v, n);
+    imprime_vetor_racionais(v, n); //imprime vetor atual
+
+    /*Elimina os valores invalidos e imprime*/
     elimina_invalidos (v, &n);
     imprime_vetor_racionais(v, n);
+
+    /*Ordena vetor e imprime*/
+    selection_sort(v, n);
+    imprime_vetor_racionais(v, n);
+
+    soma = cria_r (0, 0); //aloca memoria para a soma e inicializa com 0
+
+    for (j = 0; j < n; j++){
+        soma = soma_r (v[j], soma);
+    }
+    imprime_r(soma);
+    destroi_r(&soma);
+
+
+   /* printf("#####   TESTE DAS OPERACOES    #####\n");
+    printf(" COMPARA \n");
+
+    if (n > 1) {
+    imprime_r(v[0]);
+    printf(" ");
+    imprime_r(v[1]);
+    printf(" ");
+    int compara = compara_r (v[0], v[1]);
+    printf("%d\n", compara);
+    }
+    else printf ("vetor com n < 1\n");
+    
+    printf("\n");
+    printf("\n");
+    printf("SOMA\n");
+    if(n > 1){
+    struct racional *soma = soma_r (v[0], v[1]);
+    imprime_r(soma);
+    printf("\n");
+    }
+    
+    */
+    for (i = 0; i < n; i++)
+        destroi_r(&v[i]);
+    
+    free(v);
     return 0;
 }
