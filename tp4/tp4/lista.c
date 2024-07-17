a30e23d226f9e17ffeb453ef463b9f30b8896c15
alessandrafialla <alessandrafialla@ufpr.br>
2024-07-16 23:40:37 -0300

    Trabalho Finalizado

--- tp4/tp4/lista.c
+++ tp4/tp4/lista.c
@@ -1,93 +1,122 @@
 #include "lista.h"
 #include <stdlib.h>
-/* coloque demais includes aqui */
 
-struct lista *lista_cria (){
-
-    struct lista *l = (struct lista *) malloc(sizeof(struct lista));
+struct lista *lista_cria()
+{
+    struct lista *l = (struct lista *)malloc(sizeof(struct lista));
 
     if (l == NULL)
         return NULL;
-    
-    /*Inicializa os nodos e tamanho com NULL*/
+
+    /* Inicializa os nodos e tamanho com NULL*/
     l->ini = NULL;
-    l->ptr =NULL;
+    l->ptr = NULL;
     l->tamanho = 0;
 
     return l;
 }
-/*
-void lista_destroi (struct lista **lista){
+
+void lista_destroi(struct lista **lista)
+{
+    struct nodo *aux, *temp;
+
+    /* Se a lista for vazia */
+    if (*lista == NULL)
+        return;
+
+    aux = (*lista)->ini;
+
+    /* Percorre a lista e libera cada nodo */
+    while (aux != NULL)
+    {
+        temp = aux;
+        aux = aux->prox;
+        free(temp);
+    }
+
+    /* Libera a estrutura da lista */
+    free(*lista);
+    *lista = NULL;
 }
-*/
-int lista_insere_inicio (struct lista *lista, int chave){
+
+int lista_insere_inicio(struct lista *lista, int chave)
+{
     struct nodo *novo;
 
-    novo = (struct nodo *) malloc(sizeof(struct nodo));
+    novo = (struct nodo *)malloc(sizeof(struct nodo));
 
-    if(novo == NULL)
+    if (novo == NULL)
         return 0;
 
     novo->chave = chave;
 
-    if (lista_vazia(lista)){
+    if (lista_vazia(lista))
         novo->prox = NULL;
-        lista->ini = novo;
-        (lista->tamanho)++;
-    }
     else {
         novo->prox = lista->ini;
-        lista->ini = novo;
-        (lista->tamanho)++;
     }
-    return 1;
-}
-
-int lista_insere_fim (struct lista *lista, int chave){
-    struct nodo *novo;
-    struct nodo *aux;
-
-    /*Se a lista estiver vazia insere no inicio*/
-    if (lista_vazia(lista)){
-        lista_insere_inicio(lista, chave);
-        return 1;
-    }
-
-    novo = (struct nodo *) malloc(sizeof(struct nodo));
-
-    if(novo == NULL)
-        return 0;
-
-    novo->chave = chave;
-    novo->prox = NULL;
-    
-    aux = lista->ini;
-    while (aux->prox != NULL)
-        aux = aux->prox;
-
-    aux->prox = novo;
+    lista->ini = novo;
     (lista->tamanho)++;
     return 1;
 }
 
+int lista_insere_fim(struct lista *lista, int chave)
+{
+    struct nodo *novo;
+    struct nodo *aux;
 
-int lista_insere_ordenado (struct lista *lista, int chave){
+    /* Se a lista estiver vazia insere no inicio*/
+    if (lista_vazia(lista))
+    {
+        lista_insere_inicio(lista, chave);
+        return 1;
+    }
+
+    novo = (struct nodo *)malloc(sizeof(struct nodo));
+
+    if (novo == NULL)
+        return 0;
+
+    novo->chave = chave;
+    novo->prox = NULL;
+
+    aux = lista->ini; 
+    while (aux->prox != NULL) /* Percorre ate chegar no ultimo atual*/
+        aux = aux->prox;
+
+    aux->prox = novo; 
+    (lista->tamanho)++;
+    return 1;
+}
+
+int lista_insere_ordenado(struct lista *lista, int chave)
+{
     struct nodo *novo, *aux;
 
-    /*Se a lista estiver vazia insere no inicio*/
-    if (lista_vazia(lista)){
+    /* Se a lista estiver vazia insere no inicio*/
+    if (lista_vazia(lista))
         return lista_insere_inicio(lista, chave);
-    }
-        
-    novo = (struct nodo *) malloc(sizeof(struct nodo));
-    
-    if(novo == NULL)
+
+    novo = (struct nodo *)malloc(sizeof(struct nodo));
+
+    if (novo == NULL)
         return 0;
-        
+
+    /* Se o novo elemento deve ser inserido antes do primeiro elemento*/
+    if (lista->ini->chave >= chave)
+    {
+        novo->chave = chave;
+        novo->prox = lista->ini;
+        lista->ini = novo;
+        return 1;
+    }
+
     aux = lista->ini;
-    
-    while (aux->prox != NULL && aux->chave <= chave){
-    	aux = aux->prox;
+
+    /* Encontra a posição correta para inserir o novo nodo*/
+    while (aux->prox != NULL && aux->prox->chave <= chave)
+    {
+        aux = aux->prox;
     }
 
     novo->chave = chave;
@@ -96,32 +125,31 @@ int lista_insere_ordenado (struct lista *lista, int chave){
     return 1;
 }
 
+int lista_remove_inicio(struct lista *lista, int *chave)
+{
+    struct nodo *aux;
 
-int lista_remove_inicio (struct lista *lista, int *chave){
-    struct nodo *n;
-
-    if (lista_vazia(lista)){
+    if (lista_vazia(lista))
         return 0;
-    }
-    else {
-        *chave = lista->ini->chave;
-        n = lista->ini->prox; /*salva o segundo elemento da lista*/
-        free(lista->ini);
-        lista->ini = n;
-        (lista->tamanho)--;
-    }
+    
+    *chave = lista->ini->chave;
+    aux = lista->ini->prox; /* salva o segundo elemento da lista*/
+    free(lista->ini);
+    lista->ini = aux;
+    (lista->tamanho)--;
     return 1;
 }
 
-int lista_remove_fim (struct lista *lista, int *chave){
+int lista_remove_fim(struct lista *lista, int *chave)
+{
     struct nodo *aux;
 
-    if (lista_vazia(lista)){
+    if (lista_vazia(lista))
         return 0;
-    }
 
-    /*Se a lista tiver apenas 1 elemento*/
-    if (lista->ini->prox == NULL){
+    /* Se a lista tiver apenas 1 elemento*/
+    if (lista->ini->prox == NULL)
+    {
         *chave = lista->ini->chave;
         free(lista->ini);
         lista->ini = NULL;
@@ -130,7 +158,7 @@ int lista_remove_fim (struct lista *lista, int *chave){
 
     aux = lista->ini;
 
-    while(aux->prox->prox != NULL)
+    while (aux->prox->prox != NULL) /* Percorre ate achar o ultimo*/
         aux = aux->prox;
 
     *chave = aux->prox->chave;
@@ -140,33 +168,76 @@ int lista_remove_fim (struct lista *lista, int *chave){
     return 1;
 }
 
-/*
-int lista_remove_ordenado (struct lista *lista, int chave){
+int lista_remove_ordenado(struct lista *lista, int chave)
+{
+    struct nodo *aux, *anterior;
+
+    if (lista_vazia(lista))
+        return 0;
+
+    aux = lista->ini;
+
+    /* Se o primeiro elemento eh o desejado*/
+    if (aux->chave == chave)
+    {
+        lista->ini = aux->prox;
+        free(aux);
+        lista->tamanho--;
+        return 1;
+    }
+
+    /* Procura o elemento */
+    while (aux != NULL && aux->chave != chave)
+    {
+        anterior = aux;
+        aux = aux->prox;
+    }
+
+    anterior->prox = aux->prox;
+    free(aux);
+    lista->tamanho--;
+
+    return 1;
 }
-*/
-int lista_vazia (struct lista *lista){
+
+int lista_vazia(struct lista *lista)
+{
     return (lista->ini == NULL);
 }
 
-int lista_tamanho (struct lista *lista){
+int lista_tamanho(struct lista *lista)
+{
     return lista->tamanho;
 }
 
-/*
-int lista_pertence (struct lista *lista, int chave){
-}
-*/
+int lista_pertence(struct lista *lista, int chave)
+{
+    struct nodo *aux;
 
-void lista_inicia_iterador (struct lista *lista){
+    if ((lista_vazia(lista)))
+        return 0;
+
+    aux = lista->ini;
+
+    /* Percorre ate chegar ao fim da lista ou achar o elemento*/
+    while (aux->chave != chave && aux->prox != NULL)
+        aux = aux->prox;
+
+    /* Retorna 1 se encontrou e 0 se chegou ao fim sem encontrar*/
+    return (aux->chave == chave);
+}
+
+void lista_inicia_iterador(struct lista *lista)
+{
     lista->ptr = lista->ini;
 }
 
-int lista_incrementa_iterador (struct lista *lista, int *chave){
-    if (lista->ptr == NULL)
+int lista_incrementa_iterador(struct lista *lista, int *chave)
+{
+    if (lista->ptr == NULL) /* Lista vazia*/
         return 0;
-    else {
-        *chave = lista->ptr->chave;
-        lista->ptr = lista->ptr->prox;
-    }
+
+    *chave = lista->ptr->chave;
+    lista->ptr = lista->ptr->prox;
     return 1;
 }
--- tp4/tp4/tp4.c
+++ tp4/tp4/tp4.c
@@ -7,6 +7,23 @@
 #include "lista.h"
 #define MAX 5
 
+void print_lista (struct lista *l){
+
+    struct nodo *aux;
+
+    if (lista_vazia (l)){
+        printf ("lista vazia\n");
+        return;
+    }
+
+    aux = l->ini;
+    while (aux->prox != NULL){
+        printf ("%d ", aux->chave);
+        aux = aux->prox;
+    }
+    printf ("%d\n", aux->chave);
+}
+
 void imprimir_lista (struct lista *l){
     int chave;
 
@@ -16,10 +33,11 @@ void imprimir_lista (struct lista *l){
     }
 
     lista_inicia_iterador (l);
+    lista_incrementa_iterador (l, &chave);
+    printf ("%d", chave);
     while (lista_incrementa_iterador (l, &chave))
-        printf ("%d ", chave);
-    printf("\n");
-    /*printf ("%d\n", chave);*/
+        printf (" %d", chave);
+    printf ("\n");
 }
 
 struct lista *teste_criar_lista ()
@@ -50,6 +68,7 @@ void teste_insere_inicio (struct lista *l, int limite)
             printf ("Lista cheia, nao inseriu.\n");
             return;
         }
+        /*imprimir_lista (l);*/
         imprimir_lista (l);
     }
 }
@@ -64,6 +83,7 @@ void teste_insere_fim (struct lista *l, int limite)
             printf ("Lista cheia, nao inseriu.\n");
             return;
         }
+        /*imprimir_lista (l);*/
         imprimir_lista (l);
     }
 }
@@ -75,22 +95,28 @@ void teste_remove_inicio (struct lista *l)
     /* remove todo mundo */
     while (! lista_vazia (l))
         if (lista_remove_inicio (l, &chave))
+            /*imprimir_lista (l);*/
             imprimir_lista (l);
 }
 
 void teste_remove_fim (struct lista *l)
 {
     int chave;
-    
+
     /* remove todo mundo */
     while (! lista_vazia (l))
         if (lista_remove_fim (l, &chave))
+            /*imprimir_lista (l);*/
             imprimir_lista (l);
 }
 
-
 void teste_insere_ordenado (struct lista *l)
 {
+    /*lista_insere_ordenado (l, 13); imprimir_lista (l);
+    lista_insere_ordenado (l, 15); imprimir_lista (l);
+    lista_insere_ordenado (l, 11); imprimir_lista (l);
+    lista_insere_ordenado (l, 12); imprimir_lista (l);
+    lista_insere_ordenado (l, 14); imprimir_lista (l);*/
     lista_insere_ordenado (l, 13); imprimir_lista (l);
     lista_insere_ordenado (l, 15); imprimir_lista (l);
     lista_insere_ordenado (l, 11); imprimir_lista (l);
@@ -98,9 +124,14 @@ void teste_insere_ordenado (struct lista *l)
     lista_insere_ordenado (l, 14); imprimir_lista (l);
 }
 
-/*
+
 void teste_remove_ordenado (struct lista *l)
-{
+{ 
+    /*lista_remove_ordenado (l, 13); imprimir_lista (l);
+    lista_remove_ordenado (l, 15); imprimir_lista (l);
+    lista_remove_ordenado (l, 11); imprimir_lista (l);
+    lista_remove_ordenado (l, 12); imprimir_lista (l);
+    lista_remove_ordenado (l, 14); imprimir_lista (l);*/ 
     lista_remove_ordenado (l, 13); imprimir_lista (l);
     lista_remove_ordenado (l, 15); imprimir_lista (l);
     lista_remove_ordenado (l, 11); imprimir_lista (l);
@@ -114,7 +145,8 @@ void teste_de_pertinencia (struct lista *l)
     lista_insere_inicio (l, 15);
     lista_insere_inicio (l, 11);
     lista_insere_inicio (l, 12);
-    lista_insere_inicio (l, 14);
+    lista_insere_inicio (l, 14);  
+    /*imprimir_lista (l);*/
     imprimir_lista (l);
     if (lista_pertence (l, 11)) printf ("11 pertence\n");
     if (lista_pertence (l, 12)) printf ("12 pertence\n");
@@ -124,7 +156,6 @@ void teste_de_pertinencia (struct lista *l)
     if (! lista_pertence (l, 10)) printf ("10 nao pertence\n");
     if (! lista_pertence (l, 16)) printf ("16 nao pertence\n");
 }
-*/
 
 int main (void)
 {
@@ -136,13 +167,13 @@ int main (void)
     l = teste_criar_lista ();
     teste_lista_vazia (l);
     printf ("\n\n");
-    
+
     printf ("Teste 2: tenta remover elemento com lista vazia:\n");
     printf ("Esperado: nao pode ter dado segfault\n");
     if (! lista_remove_inicio (l, &chave))
         printf ("\tOK: remover da lista vazia nao deu segfault.\n");
     printf ("\n\n");
-    
+
     printf ("Teste 3: inserir 5 elementos no inicio:\n");
     printf ("Esperado: tem que imprimir a lista 5 vezes, a cada vez com o maior elemento no inicio. \n");
     printf ("\nATENCAO: se nao imprimir a lista corretamente o erro pode estar:\n");
@@ -151,31 +182,30 @@ int main (void)
     printf ("- voce pode implementar TEMPORARIAMENTE uma funcao que imprime a lista acessando os nodos diretamente, ate encontrar o problema.\n");
     teste_insere_inicio (l, MAX); 
     printf ("\n\n");
-    
+
     printf ("Teste 4: esvaziar a lista retirando do inicio:\n");
     printf ("Esperado: tem que imprimir a lista 5 vezes, a cada vez sem o primeiro elemento\n");
     printf ("          Ao final, mostrar a mensagem 'lista vazia'\n");
     teste_remove_inicio (l); 
     printf ("\n\n");
-    
+
     printf ("Teste 5: inserir 5 elementos no fim:\n");
     printf ("Esperado: tem que imprimir a lista 5 vezes, a cada vez com o maior elemento no fim\n");
     teste_insere_fim (l, MAX); 
     printf ("\n\n");
-    
+
     printf ("Teste 6: esvaziar a lista retirando do fim:\n");
     printf ("Esperado: tem que imprimir a lista 5 vezes, a cada vez sem o maior elemento\n");
     printf ("          Ao final, mostrar a mensagem 'lista vazia'\n");
     teste_remove_fim (l); 
     printf ("\n\n");
-    
-    
+
     printf ("Teste 7: inserir 5 elementos em ordem:\n");
     printf ("Esperado: tem que imprimir a lista 5 vezes, sempre ordenada\n");
     printf ("Esperado: tem que mostrar remover de 11 ate 15\n");
     teste_insere_ordenado (l); 
     printf ("\n\n");
-	/*
+
     printf ("Teste 8: remover 5 elementos em ordem:\n");
     printf ("Esperado: tem que imprimir a lista 5 vezes, sempre ordenada\n");
     printf ("          Ao final, mostrar a mensagem 'lista vazia'\n");
@@ -187,19 +217,19 @@ int main (void)
     printf ("          E que 10 e 16 nao pertencem\n");
     teste_de_pertinencia (l); 
     printf ("\n\n");
-
+    
     printf ("Teste 10: destruir uma lista com elementos:\n");
     printf ("Esperado: nao pode ter leak (conferir com valdrind)\n");
     printf ("          E nao pode ter segfault\n");
     lista_destroi (&l);
     printf ("\n\n");
-
+    
     printf ("Teste 11: destruir uma lista vazia:\n");
     printf ("Esperado: nao pode ter leak (conferir com valdrind)\n");
     printf ("          E nao pode ter segfault\n");
     l = lista_cria ();
     lista_destroi (&l);
     printf ("\n\n");
-    */
+    
     return 0;
 }
